062d6f1a7bb5a9403649b7232d180ef72ef7c31f
Nico Brand <nicoj.brand@gmail.com>
2019-04-24 13:44:09 +0200

    Styled Overview
    - Added briefing text feature
    - Made layout respsonsive

--- site/templates/overview.php
+++ site/templates/overview.php
@@ -1,13 +1,44 @@
 <?php snippet("header") ?>
 
 <main id="main" role="main">
+            <a name="s-jump"></a>
 
-    <div class="container ml-md-0 my-3 my-md-5">
-        <!-- PAGE CONTENT -->
+  <div id="row" class="row my-4 flex-column-reverse flex-md-row">
+      <div class="col-11 col-md-6 offset-md-1 col-lg-7 offset-lg-1 col-xl-6 offset-xl-1">
+          <h2 class="d-none d-sm-block "><?= $page->major()->html() ?>  – <?= $page->course()->html() ?></h2>
+          <h1 class="d-none d-sm-block "><?= $page->description()->html() ?></h1>
+          <?php foreach($page->parent()->authors()->toStructure() as $author): ?>
+          <?php if($author->website()->isNotEmpty()): ?>
+              <a class="font-weight-bold" href="<?= $author->website()->url() ?>" target="_blank"><span class="mr-1">&#8594;</span><?= $author->name()->html() ?></a>
+          <?php else: ?>
+              <span class="mr-1">&#8594;</span><?= $author->name()->html() ?></br>
+          <?php endif ?>
+          <?php endforeach ?>
+      </div>
+
+        <!-- Course Info & Link // back end + kirby fehlen -->
+
+      <div id="meta" class="col-11 col-md-3 col-lg-2 offset-md-1 pull-right">
+            <div class="font-weight-bold">
+                <hr align="left" class="mt-0 ml-0">
+            </div>
+            <div class="small">Supervision<br>
+                <?php foreach($page->supervisors()->split() as $supervisor): ?>
+                    <?= $supervisor ?><br>
+                <?php endforeach ?>
+                <hr align="left" class="d-md-none ml-0"><br>
+                <a class="link-briefing" href="#b-jump"><span class="mr-1">&#8595;</span> zum Briefing</a>
+            </div>
+      </div>
+
+  </div>
+
+        <!-- Side Info -->
+
         <?php if($site->hasVisibleChildren()): ?>
-        <ul id="overview-list">
+        <ul class="row"id="overview-list">
             <?php foreach($site->children()->visible() as $documentation): ?>
-                <li><a href="<?= $documentation->children()->visible()->first()->url() ?>">
+                <li ><a href="<?= $documentation->children()->visible()->first()->url()?>">
                     <?php
                     $cover = $documentation->coverImage()->toFile();
                     if ($cover) {
@@ -38,10 +69,23 @@
                 </a></li>
             <?php endforeach ?>
         </ul>
+        <!-- Briefing // back end + kirby fehlen -->
+<div id="row2" class="row my-4 flex-column-reverse flex-md-row">
+  <div id="briefing" class="col-11 col-md-6 offset-md-1 col-lg-7 offset-lg-1 col-xl-6 offset-xl-1">
+    <h5>Briefing</h5>
+    <p name="b-jump"><?= $page->text()->html() ?></p>
+  </div>
+
+  <div class="col-11 col-md-3 col-lg-2 offset-md-1 pull-right">
+          <a class="link-briefing" name="b-jump" href="#s-jump"><span class="mr-1">&#8593;</span> zur Übersicht</a>
+  </div>
+</div>
+
+
         <?php else: ?>
         <h3>You haven't added a documentation yet!</h3>
         <?php endif ?>
         <!-- PAGE CONTENT END -->
-    </div>
+
 
 </main>
