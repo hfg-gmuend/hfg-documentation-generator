53a4a4079c2a28d65d0b012aa11e3a41845f0ae8
Paul Raschke <paul.raschke@hfg-gmuend.de>
2020-11-24 08:59:41 +0100

    Remove buggy customLogin via mailserver workaround

--- site/plugins/panel-extensions/index.php
+++ site/plugins/panel-extensions/index.php
@@ -4,6 +4,3 @@
 // custom sections
 require_once(__DIR__ . "/sections/customFiles/index.php");
 require_once(__DIR__ . "/sections/customPages/index.php");
-
-// other panel extensions
-require_once(__DIR__ . "/customLogin/index.php");
