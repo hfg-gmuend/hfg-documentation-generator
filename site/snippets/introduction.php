603e4d5d472c54afae29e716d9c392d4a9737424
Paul Raschke <paul.raschke@hfg-gmuend.de>
2020-12-15 11:36:07 +0100

    Fix missing supervisors in docu introduction

--- site/snippets/introduction.php
+++ site/snippets/introduction.php
@@ -4,8 +4,8 @@
             <h1 class="d-none d-sm-block font-weight-bold"><?= $page->parent()->title()->html() ?></h1>
             <h2 class="d-block d-sm-none font-weight-bold"><?= $page->parent()->title()->html() ?></h2>
 
-            <?php foreach($page->parent()->authors()->toUsers() as $author): ?>
-            <?php if($author->website()->isNotEmpty()): ?>
+            <?php foreach ($page->parent()->authors()->toUsers() as $author): ?>
+            <?php if ($author->website()->isNotEmpty()): ?>
                 <a class="btn-link font-weight-bold" href="<?= $author->website()->url() ?>" target="_blank"><span class="mr-1">&#8594;</span><?= $author->firstName()->html() . " " . $author->lastName()->html() ?></a></br>
             <?php else: ?>
                 <span class="mr-1">&#8594;</span><?= $author->firstName()->html() . " " . $author->lastName()->html() ?></br>
@@ -32,8 +32,8 @@
                 <p class="mb-0">
                     <?php
                     $classes = [];
-                    foreach(page("overview")->classes()->split() as $class) {
-                      array_push($classes, $class);
+                    foreach (page("overview")->classes()->split() as $class) {
+                        array_push($classes, $class);
                     }
                     echo implode(", ", $classes);
                     ?>
@@ -41,7 +41,7 @@
             </div>
             <br>
             <div class="small">Supervision<br>
-                <?php foreach($page->parent()->supervisors()->split() as $supervisor): ?>
+                <?php foreach (page("overview")->supervisors()->split() as $supervisor): ?>
                     <?= $supervisor ?><br>
                 <?php endforeach ?>
                 <hr align="left" class="d-md-none ml-0">
@@ -51,7 +51,7 @@
 
     <!-- Cover Image -->
     <div class="row">
-        <?php if($page->parent()->coverImage() && $page->parent()->coverImage()->isNotEmpty() && $page->parent()->coverImage()->toFile()): ?>
+        <?php if ($page->parent()->coverImage() && $page->parent()->coverImage()->isNotEmpty() && $page->parent()->coverImage()->toFile()): ?>
             <div class="col-11 col-md-10 offset-md-1 col-lg-7">
                 <?= kirby()->kirbytag("image", $page->parent()->coverImage()->toFile()->url(), [], ["parent" => $page->parent()]) ?>
             </div>
